5a81d2e32b3103926fbf0d3d4a77fbb467c7f708
abhijan2402 <abhishek.jangid643@gmail.com>
2023-01-29 22:05:23 +0530

    adding some changes

--- Screens/PolyGon.js
+++ Screens/PolyGon.js
@@ -11,7 +11,7 @@ function PolyGon() {
 
     const GetTransactionData = async () => {
         try {
-            const response = await fetch('https://bd49-103-175-180-34.in.ngrok.io/chain', {
+            const response = await fetch('https://25aa-103-175-180-34.in.ngrok.io/chain', {
                 method: 'get',
             });
             const json = await response.json();
@@ -30,7 +30,7 @@ function PolyGon() {
             </View>
             <ScrollView>
                 <View style={styles.Table}>
-                    <View style={[styles.TableRow1, { width: windoWidth / 8 }]}>
+                    {/* <View style={[styles.TableRow1, { width: windoWidth / 8 }]}>
                         <Text style={styles.TableHeading}>Nonce</Text>
                     </View>
                     <View style={[styles.TableRow1, { width: windoWidth / 1.8 }]}>
@@ -38,23 +38,36 @@ function PolyGon() {
                     </View>
                     <View style={[styles.TableRow1, { width: windoWidth / 3.1 }]}>
                         <Text style={styles.TableHeading}>Timestamp</Text>
-                    </View>
+                    </View> */}
                 </View>
                 {
                     TrasactionData.length == 0 ? <View style={{ justifyContent: "center", alignItems: "center", height: windoHeight / 1.3 }}>
                         <ActivityIndicator size="large" color="#00ff00" />
                     </View> :
                         TrasactionData.map((item) => (
-                            <View style={styles.Table} key={item.timestamp}>
-                                <View style={[styles.TableRow1, { width: windoWidth / 8 }]}>
-                                    <Text>{item.nonce}</Text>
+                            // <View style={styles.Table} key={item.timestamp}>
+                            //     <View style={[styles.TableRow1, { width: windoWidth / 8 }]}>
+                            //         <Text>{item.nonce}</Text>
+                            //     </View>
+                            //     <View style={[styles.TableRow1, { width: windoWidth / 1.8 }]}>
+                            //         <Text>{item.previous_hash}</Text>
+                            //     </View>
+                            //     <View style={[styles.TableRow1, { width: windoWidth / 3.16 }]}>
+                            //         <Text>{item.timestamp}</Text>
+                            //     </View>
+                            // </View>
+                            <View style={{ marginHorizontal: 10, borderWidth: 1, padding: 10, borderRadius: 8, marginVertical: 5 }}>
+                                {/* <View><Text style={{ textAlign: "center", fontWeight: "800", color: "black", fontSize: 20 }}>Data</Text></View> */}
+                                <View>
+                                    <Text style={styles.NewCheckedData}>Nonce : {item.nonce}</Text>
                                 </View>
-                                <View style={[styles.TableRow1, { width: windoWidth / 1.8 }]}>
-                                    <Text>{item.previous_hash}</Text>
+                                <View>
+                                    <Text style={styles.NewCheckedData}>previous_hash: {item.previous_hash}</Text>
                                 </View>
-                                <View style={[styles.TableRow1, { width: windoWidth / 3.16 }]}>
-                                    <Text>{item.timestamp}</Text>
+                                <View>
+                                    <Text style={styles.NewCheckedData}>Timestamp : {item.timestamp}</Text>
                                 </View>
+
                             </View>
                         ))
                 }
@@ -92,6 +105,11 @@ const styles = StyleSheet.create({
     TableHeading: {
         fontWeight: "800",
         color: "black"
+    },
+    NewCheckedData: {
+        fontWeight: "800",
+        fontSize: 15,
+        marginVertical: 10
     }
 })
 export default PolyGon
